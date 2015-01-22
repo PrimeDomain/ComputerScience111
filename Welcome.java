498b345e4ea33585e3e3036530dbc76979a9a917
Grant Skinner <skinnerg@aldenv169.allegheny.edu>
2015-01-22 14:51:48 -0500

    First Practical

---
+++ Welcome.java
@@ -0,0 +1,18 @@
+//*************************************
+// Honor Code: The work I am submitting is a result of my own thinking and efforts.
+// Your Name [Replace with your name]
+// CMPSC 111 Spring 2015
+// Practical # [fill in the lab number]
+// Date: mmm dd yyyy [fill in the date]
+//
+// Purpose: ... [describe the program]
+// ***********************************
+
+public class Welcome
+{
+	public static void main ( String args[] )
+	{
+		System.out.print("Welcome to your first programming exercise ");
+		System.out.println("in CMPSC "+(100+11)+"!\n");
+	}
+}
