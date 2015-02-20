0c639d878da466e0ce1701c5210ce198a46a8071
Grant Skinner <skinnerg@allegheny.edu>
2015-02-20 13:50:43 -0500

    Added Practical4 stuff

--- Practicals/practical04/Practical4.java
+++ Practicals/practical04/Practical4.java
@@ -1,5 +1,5 @@
 //=================================================
-// Honor Code: The work I am submitting is 
+// Honor Code: The work I am submitting is
 // a result of my own thinking and efforts.
 
 // Your Name [Replace with your name]
@@ -9,17 +9,17 @@
 //
 // Purpose: ... [describe the program]
 //=================================================
-public class Practical4
+    public class Practical4
 {	public static void main(String[] args)
-	{	String s1 = "Computer Science";
-		int x = 111;
-		String s2 = s1 + " " + x;
-		String s3 = " is fun";
-		String s4 = s2 + s3;
-		
-		System.out.println("s1: " + s1);
-		System.out.println("s2: " + s2);
-		System.out.println("s3: " + s3);
-		System.out.println("s4: " + s4);
-	}
+    {	String s1 = "Computer Science";
+        int x = 111;
+        String s2 = s1 + " " + x;
+        String s3 = " is fun";
+        String s4 = s2 + s3;
+
+        System.out.println("s1: " + s1);
+        System.out.println("s2: " + s2);
+        System.out.println("s3: " + s3);
+        System.out.println("s4: " + s4);
+    }
 }
---
+++ Practicals/practical04/responses.java
@@ -0,0 +1,27 @@
+// *****************************
+// Grant Skinner
+// Practical 4 response questions
+//
+// what are five ways in which it is now different from the “default”
+// configuration that you were using previously?
+// 1. New background color and color scheme 2. using ( and { type characters will automatically
+// give you the reverse. 3. when you begin to type a word that is recognized
+// it will give you a menu that you can pick a command from.
+// 4. It remembers things you typed earlier for this as well
+// 5. It highlights things that will compile errors.
+// How does your chosen plugin work? What
+// features does it provide? Do you plan to use this
+// plugin on a regular basis? Why or why not?
+// Nerd Tree allows me to change what program I am working on using a branch
+// menu to the side of gvim. I can turn it on by typing ,e then enter on the
+// command screen. then control e toggles the nerd tree addition. I do plan
+// to use this from now on because it is helpful in navigating many tasks.
+/
+// Knowing these facts, what do you think that the “gg=G”
+// command does
+// I think it will make the top and bottom of my program have the same
+// indentation.
+//
+// How do these commands allow you to manipulate your Java program?
+// d, dd, and dip are ways of deleting lines of text in your program. U is a way
+// to quickly undo every command since the last save.
